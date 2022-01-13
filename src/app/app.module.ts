3d00473755aec803b9a2e6412a527d532102c2df
Saadat Kosumbaeva <kosumbaevasaadat@gmail.com>
2022-01-13 16:42:24 +0600

    Created basic app structure.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { RecipesComponent } from './recipes/recipes.component';
+import { RecipeComponent } from './recipes/recipe/recipe.component';
+import { EditRecipeComponent } from './recipes/edit-recipe/edit-recipe.component';
+import { ToolbarComponent } from './toolbar/toolbar.component';
+import { RecipeDetailsComponent } from './recipes/recipe-details/recipe-details.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RecipesComponent,
+    RecipeComponent,
+    EditRecipeComponent,
+    ToolbarComponent,
+    RecipeDetailsComponent
   ],
   imports: [
     BrowserModule,
